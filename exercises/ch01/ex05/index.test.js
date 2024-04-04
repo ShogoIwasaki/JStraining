68cba389a7b308bba42b9ea37b78f7455a5376e1
ShogoIwasaki <Shogo.Iwasaki@jp.ricoh.com>
2024-04-04 12:20:38 +0900

    commit

--- exercises/ch01/ex05/index.test.js
+++ exercises/ch01/ex05/index.test.js
@@ -22,26 +22,26 @@ describe("math", () => {
 
   // 以下に sum, factorial のテストを記載せよ
   describe("sum", () => {
-    it("returns the correct sum of positive numbers in an array", () => {
+    it("returns sum(6) when array([1, 2, 3]) given", () => {
       expect(sum([1, 2, 3])).toBe(6);
     });
-    it("returns 0 when an empty array is given", () => {
-      expect(sum([])).toBe(0);
-    });
-    it("returns the correct sum of negative numbers in an array", () => {
+    it("returns negative numbers sum(-6) when array([-1, -2, -3]) given", () => {
       expect(sum([-1, -2, -3])).toBe(-6);
+    });
+    it("returns sum(0) when empty array([]) given", () => {
+      expect(sum([])).toBe(0);
     });
   });
 
   describe("factorial", () => {
-    it("returns the correct factorial of a positive integer", () => {
-      expect(factorial(5)).toBe(120);
+    it("returns product(6) when positive value(3) given", () => {
+      expect(factorial(3)).toBe(6);
     });
-    it("returns 1 when 0 is given", () => {
-      expect(factorial(0)).toBe(1);
-    });
-    it("returns 1 when 1 is given", () => {
+    it("returns product(1) when 1 is given when positive value(1) given", () => {
       expect(factorial(1)).toBe(1);
+    });
+    it("returns product(1) when 0 is given when value(0) given", () => {
+      expect(factorial(0)).toBe(1);
     });
   });
 
