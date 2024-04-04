9e6324d3e380b0d0c0d863c5771c05412113682d
ShogoIwasaki <Shogo.Iwasaki@jp.ricoh.com>
2024-04-04 12:22:39 +0900

    commit

--- exercises/ch01/ex06/index.test.js
+++ exercises/ch01/ex06/index.test.js
@@ -1,11 +1,11 @@
 import { fibonacci } from "./index.js";
 
 describe("fib", () => {
-  it("returns 5 for fib(5)", () => {
+  it("returns 5 when fibonacci(5) given", () => {
     expect(fibonacci(5)).toBe(5);
   });
 
-  it("returns 2111485077978050 for fib(75)", () => {
+  it("returns 2111485077978050 when fibonacci(75) given", () => {
     expect(fibonacci(75)).toBe(2111485077978050);
   });
 });
