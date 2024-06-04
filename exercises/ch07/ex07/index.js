12b6d4961e85045fe0bdfb4f14e8a1608a032538
ShogoIwasaki <Shogo.Iwasaki@jp.ricoh.com>
2024-06-04 17:25:54 +0900

    commit

--- exercises/ch07/ex07/index.js
+++ exercises/ch07/ex07/index.js
@@ -1,23 +1,19 @@
-const data = [
-  { name: "Alice", class: "A", math: 10, chemistry: 30, geography: 20 },
-  { name: "Bob", class: "A", math: 50, chemistry: 50, geography: 60 },
-  { name: "Carol", class: "A", math: 70, chemistry: 55, geography: 30 },
-  { name: "Dave", class: "B", math: 40, chemistry: 20, geography: 60 },
-  { name: "Ellen", class: "B", math: 60, chemistry: 70, geography: 40 },
-  { name: "Frank", class: "B", math: 90, chemistry: 70, geography: 80 },
-  { name: "Isaac", class: "C", math: 70, chemistry: 40, geography: 50 },
-  { name: "Justin", class: "C", math: 80, chemistry: 40, geography: 30 },
-  { name: "Mallet", class: "C", math: 60, chemistry: 70, geography: 90 },
-];
-
-const sortedData = data.sort((a, b) => {
-  if (b.math !== a.math) {
-    return b.math - a.math;
-  } else if (b.chemistry !== a.chemistry) {
-    return b.chemistry - a.chemistry;
-  } else {
-    return b.geography - a.geography;
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
   }
-});
-
-console.log(sortedData);
+  let pivot = arr[Math.floor(arr.length / 2)];
+  let left = [];
+  let right = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i === Math.floor(arr.length / 2)) continue;
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+let n = [64, 34, 25, 12, 22, 11, 90];
+console.log(quickSort(n));
