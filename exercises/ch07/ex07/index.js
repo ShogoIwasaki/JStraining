fa3e2994bf4af71fc86613f2e66fa4a370484d03
ShogoIwasaki <Shogo.Iwasaki@jp.ricoh.com>
2024-06-04 17:50:53 +0900

    commit

--- exercises/ch07/ex07/index.js
+++ exercises/ch07/ex07/index.js
@@ -1,19 +1,19 @@
-function quickSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
-  }
-  let pivot = arr[Math.floor(arr.length / 2)];
-  let left = [];
-  let right = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (i === Math.floor(arr.length / 2)) continue;
-    if (arr[i] < pivot) {
-      left.push(arr[i]);
-    } else {
-      right.push(arr[i]);
+function bubbleSort(arr) {
+  //調べる範囲の開始位置を１つずつ後ろへ移動するfor文
+  for (var i = 0; i < arr.length; i++) {
+    //後ろから前に向かって小さい値を浮かび上がらせるfor文
+    for (var j = arr.length - 1; j > i; j--) {
+      //隣りあう２つの値を比べて、後ろが小さければ交換する
+      if (arr[j] < arr[j - 1]) {
+        var tmp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = tmp;
+      }
     }
   }
-  return [...quickSort(left), pivot, ...quickSort(right)];
+  return arr;
 }
-let n = [64, 34, 25, 12, 22, 11, 90];
-console.log(quickSort(n));
+//ソート前の配列データ
+let n = [1, 3, 10, 2, 8];
+//ソート後の配列の表示
+console.log(bubbleSort(n));
